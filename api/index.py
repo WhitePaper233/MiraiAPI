9b321823c7cd634c3325525903f6f2df9046deb6
WhitePaper233 <30739658+WhitePaper233@users.noreply.github.com>
2024-01-03 12:03:05 +0800

    Fix UA bug

--- api/index.py
+++ api/index.py
@@ -12,7 +12,7 @@ CORS(app)
 def bangumi(mid: int):
     bangumi_list = []
     page = 1
-    headers = {'User_Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'}
     while True:
         r = requests.get(
             'https://api.bilibili.com/x/space/bangumi/follow/list', 
